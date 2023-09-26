57f7f266b2d1fef22b5b7eb5399c0febf591762b
Alexey Turulin <repakuku@icloud.com>
2023-09-26 19:14:49 -0400

    27. Remove Element solved

--- 27. Remove Element.playground/Contents.swift
+++ 27. Remove Element.playground/Contents.swift
@@ -1,8 +1,13 @@
 class Solution {
     func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
-        
-        return 0
+        nums.removeAll { $0 == val }
+        return nums.count
     }
 }
 
 let solution = Solution()
+var numbers = [0, 1, 2, 2, 3, 0, 4, 2]
+let value = 2
+
+solution.removeElement(&numbers, value)
+print(numbers)
