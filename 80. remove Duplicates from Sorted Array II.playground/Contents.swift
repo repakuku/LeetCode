cc114397f93f1f07c6fbdd64e0aa6c9ed3295d01
Alexey Turulin <repakuku@icloud.com>
2023-10-05 15:16:05 -0400

    80. Remove Duplicates from Sorted Array II.

--- 80. remove Duplicates from Sorted Array II.playground/Contents.swift
+++ 80. remove Duplicates from Sorted Array II.playground/Contents.swift
@@ -1,4 +1,24 @@
 func removeDuplicates(_ nums: inout [Int]) -> Int {
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
     return nums.count
 }
+
+/*
+ 
+ [1, 1, 1, 2, 2, 3]
+ 
+ 
+ 
+ */
+
+var nums = [1, 1, 1, 2, 2, 3]
+removeDuplicates(&nums)
