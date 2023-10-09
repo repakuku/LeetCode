5e5ec35a956772f70cdedb5515e6dd123549d0f3
Alexey Turulin <repakuku@icloud.com>
2023-10-09 18:36:14 -0400

    189. Rotate Array. Solved.

--- 189. Rotate Array.playground/Contents.swift
+++ 189. Rotate Array.playground/Contents.swift
@@ -1,17 +1,32 @@
 func rotate(_ nums: inout [Int], _ k: Int) {
+    guard k > 0 else { return }
     
-    
-    
+    for _ in 0..<k {
+        var tempOne = nums[0]
+        var tempTwo = nums[nums.count - 1]
+        
+        for index in 0..<nums.count - 1 {
+            nums[index] = tempTwo
+            if index == nums.count - 2 {
+                nums[index + 1] = tempOne
+            }
+            tempTwo = tempOne
+            tempOne = nums[index + 1]
+
+        }
+    }
 }
 
 var nums = [1, 2, 3, 4, 5, 6, 7]
 var k = 3
 rotate(&nums, k)
+print(nums)
 
 nums == [5,6,7,1,2,3,4]
 
 nums = [-1,-100,3,99]
 k = 2
 rotate(&nums, k)
+print(nums)
 
 nums == [3,99,-1,-100]
