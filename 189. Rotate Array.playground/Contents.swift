d38d2c299efac7ead086240c44964e912a89a4a2
Alexey Turulin <repakuku@icloud.com>
2023-11-16 16:25:47 -0500

    189. Rotate Array. Updated.

--- 189. Rotate Array.playground/Contents.swift
+++ 189. Rotate Array.playground/Contents.swift
@@ -1,6 +1,23 @@
 func rotate(_ nums: inout [Int], _ k: Int) {
-	for _ in 0..<k {
-		nums.insert(nums.removeLast(), at: 0)
+	
+	let n = nums.count
+	let k = k % n
+	
+	reverse(&nums, start: 0, end: n - 1)
+	reverse(&nums, start: 0, end: k - 1)
+	reverse(&nums, start: k, end: n - 1)
+	
+	func reverse(_ nums: inout [Int], start: Int, end: Int) {
+		var start = start
+		var end = end
+		
+		while start < end {
+			let temp = nums[start]
+			nums[start] = nums[end]
+			nums[end] = temp
+			start += 1
+			end -= 1
+		}
 	}
 }
 
@@ -17,3 +34,8 @@ rotate(&nums, k)
 print(nums)
 
 nums == [3,99,-1,-100]
+
+// 123
+// 312
+// 231
+// 123
